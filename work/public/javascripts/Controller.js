5a519e92a6d45ba8be0e1cdf4ee62bdebd0336e8
kman007us <kevin.m.meredith@gmail.com>
2013-08-01 02:26:14 +0000

    changes ...

--- work/public/javascripts/Controller.js
+++ work/public/javascripts/Controller.js
@@ -1,3 +1,13 @@
-	function Controller($scope) { 	
-		$scope.days = ["Monday", "Tuesday", "Wednesday"];
+	var optModule = angular.module('MyModule', []);
+
+    function Controller($scope) { 	
+		$scope.days = ["", "MON", "TUE", "WED", 
+			"THU", "FRI", "SAT", "SUN"];		
+			
+		$scope.chargeNumbers =[1,2,3,4];
+			
+		$scope.addNewChargeNumber = function() { 
+			$scope.chargeNumbers.push(1);
+		}
+	
 	}
